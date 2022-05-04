06d16b866c15d9f71ae509ccacf9bfa5b96c37ce
Jonathant4242 <91383892+Jonathant4242@users.noreply.github.com>
2022-05-03 23:00:25 -0700

    mid night oil

--- lesson-2/script/currentdate.js
+++
@@ -1,4 +0,0 @@
-const options = { year: "numeric" };
-document.getElementById("currentdate").textContent = new Date().toLocaleDateString("en-UK", options);
-
-document.querySelector("#modified").textContent = document.lastModified;
