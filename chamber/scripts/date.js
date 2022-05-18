1a810b22988d3423b8bf4fb452362e10712f181d
Jonathant4242 <91383892+Jonathant4242@users.noreply.github.com>
2022-05-17 22:10:28 -0700

    error clean up

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -1,10 +1,10 @@
 // header current date
 const datefield = document.querySelector(".date")
-const datefieldUK = document.querySelector("aside");
+const datefieldUk = document.querySelector("aside");
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-Uk", { dateStyle: "full" }).format(
   now
 );
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
-datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+// datefieldUk.innerHTML = `<em>${fulldate}</em>`;
