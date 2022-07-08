fb028f764ce4b4c03d9b76060152c91a8085ed1c
John T <91383892+Jonathant4242@users.noreply.github.com>
2022-07-07 22:17:23 -0700

    updated visit_counter.js

--- chamber/scripts/visit_counter.js
+++ chamber/scripts/visit_counter.js
@@ -1,15 +1,28 @@
 // querySelectors (today) and (visits)
 const currentDate = document.querySelector(".today");
+<<<<<<< Updated upstream
 const visitsDisplay = document.querySelector(".visits");
 
 // get the stored value in localStorage
 let numVisits = Number(window.localStorage.getItem("visits-ls"));
+=======
+const totalVisits = document.querySelector(".visits");
+
+// number of visits 
+//block scoped var(let)
+//number from local storage get the stored value in localStorage
+let numVisits = Number(localStorage.getItem("visits-ls"));
+>>>>>>> Stashed changes
 
 // determine if this is the first visit or display the number of visits.
 if (numVisits !== 0) {
 	visitsDisplay.textContent = numVisits;
 } else {
+<<<<<<< Updated upstream
 	visitsDisplay.textContent = `This is your first visit!`;
+=======
+	visitsDisplay.textContent = "Welcome, thank you for visiting The City of Dracko's page";
+>>>>>>> Stashed changes
 }
 
 // increment the number of visits.
