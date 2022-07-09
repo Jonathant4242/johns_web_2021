776f32b2ca5c5ae1d8c9922ac06f437fde2c00e1
John T <91383892+Jonathant4242@users.noreply.github.com>
2022-07-09 15:23:41 -0700

    Updated Visit_counter.js
    I added a feature that allows the counsel to display when the last time someone visited in milliseconds

--- chamber/scripts/visit_counter.js
+++ chamber/scripts/visit_counter.js
@@ -1,36 +1,41 @@
 // querySelectors (today) and (visits)
 const currentDate = document.querySelector(".today");
-<<<<<<< Updated upstream
+// console.log(currentDate)
 const visitsDisplay = document.querySelector(".visits");
-
-// get the stored value in localStorage
+// console.log(visitsDisplay)
+// // get the stored value in localStorage
 let numVisits = Number(window.localStorage.getItem("visits-ls"));
-=======
-const totalVisits = document.querySelector(".visits");
 
-// number of visits 
-//block scoped var(let)
-//number from local storage get the stored value in localStorage
-let numVisits = Number(localStorage.getItem("visits-ls"));
->>>>>>> Stashed changes
+let lastVisit = Number(window.localStorage.getItem("last-visit-time"))
 
-// determine if this is the first visit or display the number of visits.
+// // determine if this is the first visit or display the number of visits.
 if (numVisits !== 0) {
 	visitsDisplay.textContent = numVisits;
 } else {
-<<<<<<< Updated upstream
 	visitsDisplay.textContent = `This is your first visit!`;
-=======
-	visitsDisplay.textContent = "Welcome, thank you for visiting The City of Dracko's page";
->>>>>>> Stashed changes
 }
 
-// increment the number of visits.
+// // increment the number of visits.
 
-// how should this be improved?
+// // how should this be improved?
 numVisits++;
-// store the new number of visits value
+// // store the new number of visits value
 localStorage.setItem("visits-ls", numVisits);
 
-// show todays date.
-currentDate.textContent = Date.now();
+// // show todays date.
+currentDate.textContent = Number(Date.now());
+
+localStorage.setItem("last-visit-time", Date.now());
+
+var timeLapse = Date.now() - lastVisit;
+
+
+
+// // timeLapse.textContent = timeLapseDisplay;
+
+// const timeLapseDisplay = document.querySelector(".previous_visit");
+
+console.log("time since last visit " + (timeLapse) +" in millisconds")
+
+
+
